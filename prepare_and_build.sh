d5b79d6270d42274a7d3a250184550495d50d83e
Timofey Turenko <timofey.turenko@mariadb.com>
2015-08-28 13:57:55 +0300

    add several attempts for avgant up

--- prepare_and_build.sh
+++ prepare_and_build.sh
@@ -3,6 +3,7 @@
 set -x
 
 export work_dir=`pwd`
+export target=`echo $target | sed "s/ //g"`
 
 #genereting json file 
 
@@ -26,10 +27,10 @@ else
 echo "rhel5 rhel6 rhel7 sles11 sles12 centos7 fedora19 fedora20 fedora21 fedora22 fedora23 deb_jessie ubuntu_vivid" | grep $box 
 if [ $? == 0 ] ; then
 	cp ~/build-scripts/build.aws.json.template ~/mdbci/build_$box.json
-	provider="--provider=aws"
+	export provider="--provider=aws"
 else
 	cp ~//build-scripts/build.json.template ~/mdbci/build_$box.json
-	provider=""
+	export provider=""
 fi
 sed -i "s/###box###/$box/g" ~/mdbci/build_$box.json
 
@@ -50,7 +51,7 @@ fi
 # starting VM for build
 ./mdbci --override --template ~/mdbci/build_$box.json generate build_conf_$box
 cd build_conf_$box
-vagrant up $provider
+~/build-scripts/vagrant_up
 if [ $? != 0 ] ; then
 	echo "Error starting VM"
 	vagrant destroy -f
