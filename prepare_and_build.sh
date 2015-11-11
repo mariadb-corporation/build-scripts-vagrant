2420a66a5eb83e78cb9df9f05aa8d05305587450
Timofey Turenko <timofey.turenko@mariadb.com>
2015-11-11 15:49:38 +0100

    unique name for build_conf

--- prepare_and_build.sh
+++ prepare_and_build.sh
@@ -26,9 +26,11 @@ else
 cd ~/mdbci
 
 provider=`./mdbci show provider $box --silent 2> /dev/null`
-cp ~/build-scripts/build.$provider.json.template ~/mdbci/build_$box.json
+datestr=`date +%Y%m%d-%H%M`
+name=`echo build_$box_$datestr`
+cp ~/build-scripts/build.$provider.json.template ~/mdbci/$name.json
 
-sed -i "s/###box###/$box/g" ~/mdbci/build_$box.json
+sed -i "s/###box###/$box/g" ~/mdbci/$name.json
 
 while [ -f ~/vagrant_lock ]
 do
@@ -37,15 +39,15 @@ done
 touch ~/vagrant_lock
 
 # destroying existing box
-if [ -d "build_conf_$box" ]; then
-	cd build_conf_$box
+if [ -d "$name" ]; then
+	cd $name
 	vagrant destroy -f
 	cd ..
 fi
 
 # starting VM for build
-./mdbci --override --template ~/mdbci/build_$box.json generate build_conf_$box
-./mdbci up --attempts=4 build_conf_$box
+./mdbci --override --template ~/mdbci/$name.json generate $name
+./mdbci up --attempts=4 $name
 if [ $? != 0 ] ; then
 	echo "Error starting VM"
 	vagrant destroy -f
@@ -53,11 +55,11 @@ if [ $? != 0 ] ; then
 	exit 1
 fi
 
-export sshuser=`./mdbci ssh --command 'whoami' --silent build_conf_$box/build 2> /dev/null`
+export sshuser=`./mdbci ssh --command 'whoami' --silent $name/build 2> /dev/null`
 
 # get VM info
-export IP=`./mdbci show network build_conf_$box/build --silent 2> /dev/null`
-export sshkey=`./mdbci show keyfile build_conf_$box/build --silent 2> /dev/null`
+export IP=`./mdbci show network $name/build --silent 2> /dev/null`
+export sshkey=`./mdbci show keyfile $name/build --silent 2> /dev/null`
 export scpopt="-i $sshkey -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "
 export sshopt="$scpopt $sshuser@$IP"
 
@@ -67,9 +69,12 @@ rm ~/vagrant_lock
 cd $work_dir
 ~/build-scripts/build.sh
 res=$?
-cd ~/mdbci/build_conf_$box
+cd ~/mdbci/$name
 if [ "x$do_not_destroy_vm" != "xyes" ] ; then
 	vagrant destroy -f
+	cd ..
+	rm -rf $name
+	rm -rf $name.json
 fi
 exit $res
 
