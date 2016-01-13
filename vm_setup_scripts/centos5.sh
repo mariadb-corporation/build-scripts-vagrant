8a4b0a1c7358126a014a16853275584a0f86fa0f
Timofey Turenko <timofey.turenko@mariadb.com>
2016-01-13 15:31:35 +0100

    add EPEL repo for rhel/centos 5 machines

--- vm_setup_scripts/centos5.sh
+++ vm_setup_scripts/centos5.sh
@@ -1,6 +1,6 @@
 yum install -y wget
-#wget http://download.fedoraproject.org/pub/epel/5/i386/epel-release-5-4.noarch.rpm
-#rpm -Uvh epel-release-5-4.noarch.rpm
+wget http://download.fedoraproject.org/pub/epel/5/i386/epel-release-5-4.noarch.rpm
+rpm -Uvh epel-release-5-4.noarch.rpm
 
 yum install -y gcc gcc-c++ make
 wget http://www.cmake.org/files/v2.8/cmake-2.8.12.2.tar.gz --no-check-certificate
