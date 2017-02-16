dac595cfc8d107cac9821c07baeb4972ea8e9ff3
Timofey Turenko <timofey.turenko@mariadb.com>
2017-02-16 15:05:35 +0100

    #7975 now variable does not have path in it

--- copy_repos/generate_build_info_path.sh
+++ copy_repos/generate_build_info_path.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 
+$web_prefix=$(echo $path_prefix | sed 's|/home/vagrant/repository/||g')
+
 if [ "$image_type" == "RPM" ] ; then
-        build_info_path="$path_prefix/$platform/$platform_version/$arch/build_info"
+        build_info_path="$web_prefix/$platform/$platform_version/$arch/build_info"
 else
-        build_info_path="$path_prefix/$platform_family/dists/$platform_version/main/binary-$arch/build_info"
+        build_info_path="$web_prefix/$platform_family/dists/$platform_version/main/binary-$arch/build_info"
 fi
 
 echo "BUILD_PATH_INFO=$build_info_path" > $WORKSPACE/build_info_env_var_$BUILD_ID
