621ad2aa077244aa83877afe063973b93b02d8bc
Timofey Turenko <timofey.turenko@skysql.com>
2017-02-18 01:31:49 +0200

    fix web and filesys paths

--- copy_repos/copy_repos.sh
+++ copy_repos/copy_repos.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-. $HOME/build-scripts/copy_repos/generate_build_info_path.sh
-
 if [ "$image_type" == "RPM" ] ; then
         export arch=`ssh $sshopt "arch"`
         . $HOME/build-scripts/copy_repos/generate_build_info_path.sh
--- copy_repos/generate_build_info_path.sh
+++ copy_repos/generate_build_info_path.sh
@@ -2,12 +2,14 @@
 
 set -x
 
-web_prefix=$(echo $path_prefix | sed "s|$HOME/repository/||g")
+export web_prefix=$(echo $path_prefix | sed "s|$HOME/repository/||g")
 
 if [ "$image_type" == "RPM" ] ; then
-        export build_info_path="$web_prefix/$platform/$platform_version/$arch/build_info"
+        export build_info_file="$platform/$platform_version/$arch/build_info"
 else
-        export build_info_path="$web_prefix/$platform_family/dists/$platform_version/main/binary-$arch/build_info"
+        export build_info_file="$platform_family/dists/$platform_version/main/binary-$arch/build_info"
 fi
 
-echo "BUILD_PATH_INFO=$web_prefix" > $WORKSPACE/build_info_env_var_$BUILD_ID
+echo "BUILD_PATH_INFO=$web_prefix/$build_info_file" > $WORKSPACE/build_info_env_var_$BUILD_ID
+
+export build_info_path=$path_prefix/$build_info_file
