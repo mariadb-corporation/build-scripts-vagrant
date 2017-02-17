55e03336833c5f999beed2f9bc07b463311feb70
Alexander Kaluzhniy <GALIAF1995@GMAIL.COM>
2017-02-17 20:57:39 +0300

    #7957 added env ver for build path generation (#47)
    * #7957 added env ver for build path generation
    
    * #7975 fixes
    
    * #7975 now variable does not have path in it
    
    * #7975 fixes
    
    * #7975 added debug info
    
    * fix build_info_path and use it in copy_repos.sh

---
+++ copy_repos/generate_build_info_path.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+set -x
+
+web_prefix=$(echo $path_prefix | sed 's|$HOME/repository/||g')
+
+if [ "$image_type" == "RPM" ] ; then
+        export build_info_path="$web_prefix/$platform/$platform_version/$arch/build_info"
+else
+        export build_info_path="$web_prefix/$platform_family/dists/$platform_version/main/binary-$arch/build_info"
+fi
+
+echo "BUILD_PATH_INFO=$build_info_path" > $WORKSPACE/build_info_env_var_$BUILD_ID
