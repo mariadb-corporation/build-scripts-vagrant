0eb663655a85853d67735e71e1ac9ee68fc840a3
Timofey Turenko <timofey.turenko@mariadb.com>
2017-03-24 16:43:19 +0200

    Local deployment (#52)
    * add local deployment stuff
    
    * change default to centos_7_docker
    
    * add docker and libvirt local deployment scripts
    
    * remove parameters setting from create_local_config.sh

---
+++ test/create_local_config_docker.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export version="10.1"
+export product="mariadb"
+export box="centos_7_docker"
+export do_not_destroy_vm="yes"
+export big="no"
+export ci_url="http://max-tst-01.mariadb.com/ci-repository/"
+export ci_url_suffix="mariadb-maxscale"
+export repo_user=""
+export repo_password=""
+export logs_dir="$HOME/LOGS"
+export vm_memory="512"
+
+. ~/build-scripts/test/create_local_config.sh $*
