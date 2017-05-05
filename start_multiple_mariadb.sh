5be41f163d26c01b23b5f61d170580a52d4a30d5
Timofey Turenko <timofey.turenko@skysql.com>
2017-05-05 14:24:02 +0300

    script to start 8 servers

---
+++ start_multiple_mariadb.sh
@@ -0,0 +1,33 @@
+set -x
+
+servers=8;
+dir=`pwd`
+
+cp ~/build-scripts/test/multiple_servers.cnf $dir
+sudo rm -rf /data/mysql/*
+sudo rm -rf /var/log/mysql/*
+sudo mkdir -p /data/mysql
+sudo chown mysql:mysql -R /data
+sudo mkdir -p /var/run/mysqld
+sudo chown mysql:mysql -R /var/run/mysqld
+sudo killall mysqld
+sudo killall mysql_install_db
+
+for i in `seq 1 $servers`;
+do
+    sudo mysql_install_db --defaults-file=$dir/multiple_servers.cnf --user=mysql --datadir=/data/mysql/mysql$i 
+done
+
+sudo mysqld_multi  --defaults-file=$dir/multiple_servers.cnf  start --no-log --verbose &
+
+running_servers=0
+while [ $running_servers != $servers ] ; do
+   running_servers=`mysqld_multi --defaults-file=/home/turenko/m/multiple_servers.cnf report | grep "is running" | wc -l`
+done
+
+
+for i in `seq 1 $servers`;
+do
+    sudo mysql --socket=/var/run/mysqld/mysqld$i.sock < ~/build-scripts/test-setup-scripts/create_repl_user.sql
+    sudo mysql --socket=/var/run/mysqld/mysqld$i.sock < ~/build-scripts/test-setup-scripts/create_skysql_user.sql
+done
